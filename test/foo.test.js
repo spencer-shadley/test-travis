e0d0e6874473254e30d57059ba96879758d660e0
Spencer Shadley <spencershadley@utexas.edu>
2020-03-01 21:48:16 -0800

    added fake test

--- test/foo.test.js
+++ test/foo.test.js
@@ -1,8 +1,14 @@
+'use strict';
+
+const add = require('../src/add');
+const subtract = require('../src/subtract');
+
 describe(`jest test`, () => {
-  it(`should pass`, () => {
-    expect(1 + 2).toEqual(3);
+  it(`add`, () => {
+    expect(add.add(1, 2)).toEqual(3);
   });
-  it('should fail', () => {
-    expect('1' + '1').toEqual(2);
+
+  it(`subtract`, () => {
+    expect(subtract.subtract(1, 2)).toEqual(-1);
   });
 });
